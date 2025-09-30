0d45bdb1211b0cbda555ca4110707451d03cfcf9
SaNya51200~ <sssanechek0@gmail.com>
2025-09-30 12:06:57 +0300

    Add a new greeting module with user input

---
+++ greeting.py
@@ -0,0 +1,39 @@
+def greet():
+    name = input("Введите ваше имя: ")
+    print(f"Привет, {name}!")
+
+if __name__ == "__main__":
+    greet()  GNU nano 8.5                      greeting.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            [ line  1/1 (100%), col  1/ 1 (100%), char  0/0 ( 0%) ]
+^G Help      ^O Write Out ^F Where Is  ^K Cut       ^T Execute   ^C Location
+^X Exit      ^R Read File ^\ Replace   ^U Paste     ^J Justify   ^/ Go To Line
